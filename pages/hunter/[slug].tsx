76001497f317a74a4fdfa811479553ab9136b867
Joydeep Singha <joydeepsingha68@gmail.com>
2022-03-27 20:44:46 +0530

    fix: bug fixes

--- pages/hunter/[slug].tsx
+++ pages/hunter/[slug].tsx
@@ -15,15 +15,16 @@ const Hunter = () => {
   const router = useRouter();
   const { slug } = router.query;
   const [address, setAddress] = useState<undefined | string>();
-  const [nfts, setNfts] = useState< any[]>([]);
-  const [image,setImage]=useState("https://testnets.opensea.io/static/images/placeholder.png")
+  const [nfts, setNfts] = useState<any[]>([]);
+  const [image, setImage] = useState(
+    "https://testnets.opensea.io/static/images/placeholder.png"
+  );
   const [isHunterDomain, setIsHunterDomain] = useState(false);
-  const [profileNft,setProfileNft]=useState<any>();
-  const { account,web3Provider } = useWallet();
-
+  const [profileNft, setProfileNft] = useState<any>();
+  const { account, web3Provider } = useWallet();
   const fetchAddress = useCallback(async () => {
     try {
-      if ( slug) {
+      if (slug) {
         const signer = web3Provider.getSigner();
         const contract = new ethers.Contract(
           hunterDomainAddress,
@@ -34,41 +35,50 @@ const Hunter = () => {
         const owner = await contract.domains(name.replace(".hunter", ""));
         setAddress(owner);
         setIsHunterDomain(true);
-        fetchHunterMetadata()
+        fetchHunterMetadata();
       }
     } catch (error) {
       console.log(error);
     }
-  }, [slug,web3Provider]);
-
+  }, [slug, web3Provider]);
 
   const fetchHunterMetadata = useCallback(async () => {
-    if(address&&isHunterDomain&&typeof slug === "string"){
+    if (address && isHunterDomain && typeof slug === "string") {
       const nfts = await web3.alchemy.getNfts({
         owner: address,
         contractAddresses: [hunterDomainAddress],
       });
-      console.log("NFTs", nfts.ownedNfts.filter((nft) => nft.title.replace(".hunter", "") === slug.replace(".hunter", ""))[0]);
-      setProfileNft(nfts.ownedNfts.filter((nft) => nft.title.replace(".hunter", "") === slug.replace(".hunter", ""))[0])
+      console.log(
+        "NFTs",
+        nfts.ownedNfts.filter(
+          (
+            nft // @ts-ignore
+          ) => nft.title.replace(".hunter", "") === slug.replace(".hunter", "")
+        )[0]
+      );
+      setProfileNft(
+        nfts.ownedNfts.filter(
+          (
+            nft // @ts-ignore
+          ) => nft.title.replace(".hunter", "") === slug.replace(".hunter", "")
+        )[0]
+      );
     }
-  } , [address,isHunterDomain]);
-
-
-  useEffect(() => {
-    fetchHunterMetadata()
-  },[fetchHunterMetadata])
+  }, [address, isHunterDomain]);
 
   useEffect(() => {
-    if(typeof slug === "string"){
-      if(isAddress(slug)){
-        setAddress(slug)
+    fetchHunterMetadata();
+  }, [fetchHunterMetadata]);
+
+  useEffect(() => {
+    if (typeof slug === "string") {
+      if (isAddress(slug)) {
+        setAddress(slug);
       } else {
-        if(web3Provider)
-        fetchAddress();
+        if (web3Provider) fetchAddress();
       }
     }
-  }, [fetchAddress,web3Provider]);
-
+  }, [fetchAddress, web3Provider]);
 
   const getUserNft = useCallback(async () => {
     if (address) {
@@ -77,6 +87,7 @@ const Hunter = () => {
         contractAddresses: [bountyMakerAddress],
       });
       setNfts(nfts.ownedNfts);
+      // @ts-ignore
       setImage(nfts?.ownedNfts[0]?.media[0]?.gateway);
     }
   }, [address, web3]);
@@ -84,32 +95,41 @@ const Hunter = () => {
   useEffect(() => {
     getUserNft();
   }, [address, getUserNft]);
-console.log(profileNft)
+  console.log(profileNft);
   return (
     <div className="mx-4 mt-16 sm:mt-32 sm:mx-10 md:mx-40">
       {/* Hi {slug as string}, your address is: {address as string} */}
-      {
-        profileNft&&(
-      <div className="rounded-lg w-fit mb-6 md:mb-12">
-        <img className="max-w-[150px] rounded-md border-solid border-2" src={profileNft.media[0]?.gateway} alt=""/>
-        <div className="flex items-center mt-2">
-        <p className="text-3xl font-semibold text-primary-500">{profileNft?.title}</p>
-          <p className="ml-2 text-sm text-secondary-400 font-normal">{address&&getEllipsisTxt(address,3)}</p>
+      {profileNft && (
+        <div className="rounded-lg w-fit mb-6 md:mb-12">
+          <img
+            className="max-w-[150px] rounded-md border-solid border-2"
+            src={profileNft.media[0]?.gateway}
+            alt=""
+          />
+          <div className="flex items-center mt-2">
+            <p className="text-3xl font-semibold text-primary-500">
+              {profileNft?.title}
+            </p>
+            <p className="ml-2 text-sm text-secondary-400 font-normal">
+              {address && getEllipsisTxt(address, 3)}
+            </p>
+          </div>
         </div>
-      </div>
-              )
-   }
+      )}
 
-
-<div className="grid grid-cols-1 gap-y-3 gap-x-3 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-4 mt-7">
-   {
-        nfts?.map((nft) => (
-      <div className="rounded-lg p-2 border border-solid border-secondary-500 w-fit">
-      <img className="max-w-[200px] rounded-lg" src={nft?.media[0]?.gateway?nft?.media[0]?.gateway:image} alt=""/>
-        <p className="text-sm font-semibold text-secondary-500 mt-2">{nft?.title}</p>
-      </div>
-        ))
-      }
+      <div className="grid grid-cols-1 gap-y-3 gap-x-3 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-4 mt-7">
+        {nfts?.map((nft) => (
+          <div className="rounded-lg p-2 border border-solid border-secondary-500 w-fit">
+            <img
+              className="max-w-[200px] rounded-lg"
+              src={nft?.media[0]?.gateway ? nft?.media[0]?.gateway : image}
+              alt=""
+            />
+            <p className="text-sm font-semibold text-secondary-500 mt-2">
+              {nft?.title}
+            </p>
+          </div>
+        ))}
       </div>
     </div>
   );
--- utils/getBountyData.ts
+++ utils/getBountyData.ts
@@ -1,13 +1,13 @@
 import { Contract, Provider } from "ethers-multicall";
 import { BigNumber } from "@ethersproject/bignumber";
-import { bountyMakerAddress, rinkeby } from "../constants";
+import { bountyMakerAddress } from "../constants";
 import { ethers } from "ethers";
 import BountyMakerAbi from "../constants/abis/BountyMaker.json";
-const rpcProvider = new ethers.providers.JsonRpcProvider(rinkeby.rpc);
+// const rpcProvider = new ethers.providers.JsonRpcProvider(rinkeby.rpc);
 
 export default async function getBountyData() {
-  const provider = new Provider(rpcProvider);
-  await provider.init();
+  // const provider = new Provider(rpcProvider);
+  // await provider.init();
 
   const bountyMakerContract = new Contract(bountyMakerAddress, BountyMakerAbi);
 
